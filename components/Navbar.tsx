7015a8a9861d540a1d11f19b855fbce7d4061e46
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-01-20 04:41:36 -0600

    Add: estilos y estrucutra componente navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,39 @@
+import { Trabajos } from "@/constants";
+import { CheckIsDesktop } from "@/hooks/Desktop";
+import Image from "next/image";
+
+import styles from "styles/Navbar.module.css";
+
+
+// TODO: Agregar enlaces al trabajo en cuestion
 export const Navbar = () => {
+  const IsDesktop = CheckIsDesktop();
+
   return (
-    <div>
-      Navbar component works!
-    </div>
-  )
-}
+    <nav className={`container center-y ${styles.navigation}`}>
+      <section className={`container center-y ${styles.brand}`}>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Escudo_UdeG.svg"
+          width={32}
+          height={40}
+          alt="Logo Universidad de Guadalajara"
+          priority
+        />
+
+        <h1 className={styles.title}>Estructuras de datos</h1>
+      </section>
+
+      <ul className={styles.menu}>
+        {Trabajos.map((trabajo, index) => {
+          return (
+            <li key={index}>
+              <a className={styles.menuItem} href="#">
+                {trabajo}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
