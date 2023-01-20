680f073178f250816b1049112b5bfccc37c4e7a4
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-01-20 03:08:10 -0600

    Add: componentes footer navbar

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div>
+      Footer component works!
+    </div>
+  )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <div>
+      Navbar component works!
+    </div>
+  )
+}
