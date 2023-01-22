3af848be4aa0c0c07f4dd672fc3d42e94746caaf
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-01-22 02:01:40 -0600

    Add: añadir datos redes sociales

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,3 +1,5 @@
+import { RedSocial } from "@/types"
+
 export const Trabajos = [
   "Producto 1",
   "Producto 2",
@@ -5,3 +7,21 @@ export const Trabajos = [
   "Producto 4",
   "Proyecto Final"
 ]
+
+export const RedesSociales: RedSocial[] = [
+  {
+    name: "GitHub",
+    iconClass: "fa-brands fa-github",
+    url: "https://github.com/Jorgemacias-12",
+  },
+  {
+    name: "Linkedin",
+    iconClass: "fa-brands fa-linkedin",
+    url: "https://www.linkedin.com/in/JAMZ3",
+  },
+  {
+    name: "Spotify",
+    iconClass: "fa-brands fa-spotify",
+    url: "https://open.spotify.com/user/1w8d4yxalyjgitha2a12x13dk?si=832733d904094d5c",
+  },
+];
