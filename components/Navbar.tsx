cc1884567104331672d31e99e2bffe7dfc63a372
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-01-22 00:38:50 -0600

    Add: mejoras al menu
    	Ahora el menú es desplegable utilizando un
    	botón

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,55 @@
 import { Trabajos } from "@/constants";
 import { CheckIsDesktop } from "@/hooks/Desktop";
 import Image from "next/image";
+import { createRef, useState } from "react";
 
 import styles from "styles/Navbar.module.css";
-
+import { Menu } from "./Menu";
 
 // TODO: Agregar enlaces al trabajo en cuestion
 export const Navbar = () => {
   const IsDesktop = CheckIsDesktop();
 
-  return (
-    <nav className={`container center-y ${styles.navigation}`}>
-      <section className={`container center-y ${styles.brand}`}>
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Escudo_UdeG.svg"
-          width={32}
-          height={40}
-          alt="Logo Universidad de Guadalajara"
-          priority
-        />
+  const [togglerClass, setVisible] = useState(false);
 
-        <h1 className={styles.title}>Estructuras de datos</h1>
+  const openMenu = () => {
+    setVisible(!togglerClass);
+  };
+
+  return (
+    <nav
+      className={`container center-y ${styles.navigation} ${
+        togglerClass && styles.mobile
+      }`}
+    >
+      <section className={`container ${styles.brandContainer}`}>
+        <section className={`container center ${styles.brand}`}>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Escudo_UdeG.svg"
+            width={32}
+            height={40}
+            alt="Logo Universidad de Guadalajara"
+            priority
+          />
+
+          <h1 className={styles.title}>Estructuras de datos</h1>
+        </section>
+
+        {!IsDesktop && (
+          <i
+            onClick={openMenu}
+            className={`${styles.menuToggler} fas fa-${
+              togglerClass ? "times" : "bars"
+            }`}
+          ></i>
+        )}
       </section>
 
-      <ul className={styles.menu}>
-        {Trabajos.map((trabajo, index) => {
-          return (
-            <li key={index}>
-              <a className={styles.menuItem} href="#">
-                {trabajo}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      {IsDesktop ? (
+        <Menu IsVisible={false} />
+      ) : (
+        <Menu IsVisible={togglerClass} />
+      )}
     </nav>
   );
 };
