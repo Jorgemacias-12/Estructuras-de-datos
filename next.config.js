3520522e1793abbba786c81c19bff24a649af07d
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-01-27 16:53:22 -0600

    Add: cambiar basePath dependiendo entorno

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: ''
+    path: ""
   },
-  basePath: `/Estructuras-de-datos`
+  basePath: process.env.NODE_ENV === 'production' ? '/Estructura-de-datos' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-custom-path/' : '',
 }
 
 module.exports = nextConfig
