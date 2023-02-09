f1990d910dab9c1e342b5717ef0368cc5e4742f8
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-02-09 11:43:26 -0600

    Fix: el tamaño del layout
    	Utilizar grid para que el layout utilice el mayor espacio
    	disponible junto al contenido

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { Fragment, ReactNode } from "react";
 import { Footer } from "../Footer";
 import { Navbar } from "../Navbar";
+import styles from "styles/Layout.module.css"
 
 type LayoutProps = {
   children: ReactNode;
@@ -8,10 +9,10 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div className={styles.appContainer}>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
