4567c4e8378ba89a8b7e0ac97b986ec400006189
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-02-13 03:37:19 -0600

    Fix: el componente no te mandaba al contenido

--- components/contentList/contentList.tsx
+++ components/contentList/contentList.tsx
@@ -10,16 +10,21 @@ type contentListProps = {
 
 export const ContentList = ({ items }: contentListProps) => {
   const GoToList = useId();
-  const url = cleanURL();
+  const url = cleanURL(useRouter().pathname);
 
   const scrollToHeader = (event: React.MouseEvent<HTMLButtonElement>) => {
-    window.history.pushState("", document.title, window.location.pathname)
+    window.history.pushState("", document.title, window.location.pathname);
+
+    // console.log("No deberia de fallar")
 
     document.body.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
   const goToArticle = (link: string) => {
     window.location.href = url + link;
+    document
+      .getElementById(link)
+      ?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
   return (
