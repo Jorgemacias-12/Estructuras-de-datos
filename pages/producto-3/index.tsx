567847fa206b114c869d375f74a0a1ba49cb68b8
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-02-13 10:45:20 -0600

    Add: estructura de contenido UA3
    	Ademas de su CSS

--- pages/producto-3/index.tsx
+++ pages/producto-3/index.tsx
@@ -1,7 +1,11 @@
 import { ContentList } from "@/components/contentList/contentList";
+import { JEditor } from "@/components/editor/JEditor";
 import { Header } from "@/components/header/Header";
+import { Container } from "@/components/container/Container";
+import { UA3_workList } from "@/constants";
+import Editor from "@monaco-editor/react";
 import Head from "next/head";
-import styles from "styles/Producto-2.module.css";
+import styles from "styles/Producto-3.module.css";
 
 export const Producto_3 = () => {
   return (
@@ -12,64 +16,35 @@ export const Producto_3 = () => {
 
       <Header title="UA3-TDA Búsqueda y Ordenamiento" />
 
-      <ContentList items={[{ title: "Si" }]} />
+      <ContentList items={UA3_workList} />
 
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">Búsqueda lineal en estructura de datos</h2>
-      </section>
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">2</h2>
-      </section>
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">3</h2>
-      </section>
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">4</h2>
-      </section>
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">5</h2>
-      </section>
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">6</h2>
-      </section>
+      {UA3_workList.map(({ title, content, images, code, link }, index) => {
+        return (
+          <Container key={index} identity={link}>
+            <h2 className="subtitle">{title}</h2>
 
-      {/* 
-      <section className={styles.container} id="lineal">
-        <hr className="separator separator-x" />
+            {content.map((text, index) => {
+              return (
+                <p
+                  className={styles.text}
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: text }}
+                ></p>
+              );
+            })}
 
-        <h2 className="subtitle">1</h2>
-      </section>
+            <Editor
+              height="350px"
+              language="python"
+              theme="vs-dark"
+              value={code}
+              options={{ readOnly: true, fontFamily: "JetBrains Mono" }}
+            />
 
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">2</h2>
-      </section>
-
-      <se className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">3</h2>
-
-        <section className={styles.container}>
-          <hr className="separator separator-x" />
-          <h2 className="subtitle">4</h2>
-        </section>
-
-        <section className={styles.container}>
-          <hr className="separator separator-x" />
-          <h2 className="subtitle">5</h2>
-        </section>
-      </se                 ction>
-
-      <section className={styles.container}>
-        <hr className="separator separator-x" />
-        <h2 className="subtitle">6</h2> */}
-      {/* </section> */}
+            <hr className="separator separator-x" />
+          </Container>
+        );
+      })}
     </>
   );
 };
