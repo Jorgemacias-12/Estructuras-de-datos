3853af9f306e71936740ebe109e5fd151f85fcd5
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-02-13 10:35:15 -0600

    Fix: obtener el valor de la ruta por parametro

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,8 @@
-export const cleanURL = () => {
-  let route: string = window.location.href;
+export const cleanURL = (route: string) => {
+  let targetId: RegExp = /#w{1,}-{0,}w{0,}-{0,}w{0,}/;
 
-    let targetId: RegExp = /#w{1,}-{0,}w{0,}-{0,}w{0,}/;
-
-    // Limpiar url si ya había un elemento referenciado
-    route = targetId.test(route) ? route.replace(targetId, "") : "";
-    return route;
-}
+  // Limpiar url si ya había un elemento referenciado
+  route = targetId.test(route) ? route.replace(targetId, "") : "";
+  
+  return route;
+};
