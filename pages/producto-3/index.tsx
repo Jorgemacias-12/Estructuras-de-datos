4b7f6ab1eb1b4e1d444c6e5801c71a71aeb0fed5
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-02-20 02:11:29 -0600

    Add: estilos y estructura para UA3

--- pages/producto-3/index.tsx
+++ pages/producto-3/index.tsx
@@ -1,13 +1,30 @@
 import { ContentList } from "@/components/contentList/contentList";
-import { JEditor } from "@/components/editor/JEditor";
 import { Header } from "@/components/header/Header";
 import { Container } from "@/components/container/Container";
-import { UA3_workList } from "@/constants";
-import Editor from "@monaco-editor/react";
+import { colaDownloadLink, producto_2_referencias, UA3_workList } from "@/constants";
 import Head from "next/head";
 import styles from "styles/Producto-3.module.css";
+import { CodeViewer } from "@/components/codeViewer/codeViewer";
+import Cite from "@/components/cite/Cite";
+import { useEffect, useState } from "react";
+import { JEditor } from "@/components/editor/JEditor";
 
 export const Producto_3 = () => {
+  
+  const [code, setCode] = useState("");
+
+  useEffect( () => {
+    
+    const fetchData = async () => {
+      const cola = await fetch(colaDownloadLink).then((response) => response.text())
+
+      setCode(cola);
+    }
+
+    fetchData();
+
+  },[]);
+
   return (
     <>
       <Head>
@@ -23,28 +40,38 @@ export const Producto_3 = () => {
           <Container key={index} identity={link}>
             <h2 className="subtitle">{title}</h2>
 
-            {content.map((text, index) => {
-              return (
-                <p
-                  className={styles.text}
-                  key={index}
-                  dangerouslySetInnerHTML={{ __html: text }}
-                ></p>
-              );
-            })}
+            <section className={styles.container}>
+              {content.map((text, index) => {
+                return (
+                  <p
+                    className={styles.text}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                    key={index}
+                  ></p>
+                );
+              })}
+            </section>
 
-            <Editor
-              height="350px"
-              language="python"
-              theme="vs-dark"
-              value={code}
-              options={{ readOnly: true, fontFamily: "JetBrains Mono" }}
-            />
+            <CodeViewer code={code} />
 
             <hr className="separator separator-x" />
           </Container>
         );
       })}
+
+      <h2 id="code" className={styles.infoTitle}>Código completo</h2>
+
+      <CodeViewer code={code}/>
+
+      <h2 id="info" className={styles.infoTitle}>Fuentes de información</h2>
+
+      <Cite
+        title={"UA3.- BÚSQUEDA Y ORDENAMIENTO"}
+        content={"Recuperado el 20 de febrero de 2023, de"}
+        link={
+          "https://sites.google.com/academicos.udg.mx/drlizcano/il354-estructura-datos/ua3-b%C3%BAsqueda-y-ordenamiento?authuser=0"
+        }
+      />
     </>
   );
 };
