adf19a2f7247b743f77dc7770fee46110b580289
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-01-06 04:32:49 -0600

    Update: index.ts in locales folder
    	Add SOCIAL_MEDIA array of SocialMedia type

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,4 +1,24 @@
+import type { SocialMedia } from "types"
+
 export const LANGS = [
   "es",
   "en"
+]
+
+export const SOCIAL_MEDIA: SocialMedia[] = [
+  {
+    name: "GitHub",
+    icon: "fa-brands fa-github",
+    url: "https://github.com/Jorgemacias-12",
+  },
+  {
+    name: "Linkedin",
+    icon: "fa-brands fa-linkedin",
+    url: "https://www.linkedin.com/in/JAMZ3",
+  },
+  {
+    name: "Spotify",
+    icon: "fa-brands fa-spotify",
+    url: "https://open.spotify.com/user/1w8d4yxalyjgitha2a12x13dk?si=832733d904094d5c",
+  },
 ]
