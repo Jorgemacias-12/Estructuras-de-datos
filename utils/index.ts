95567fa0ed23ba6fe14da0ea76b14bc9724afb32
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-01-08 00:27:00 -0600

    Remove nextjs project
    	Im going to move the project to an astro blog app.

--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const cleanURL = (route: string) => {
-  let targetId: RegExp = /#w{1,}-{0,}w{0,}-{0,}w{0,}/;
-
-  // Limpiar url si ya había un elemento referenciado
-  route = targetId.test(route) ? route.replace(targetId, "") : "";
-  
-  return route;
-};
