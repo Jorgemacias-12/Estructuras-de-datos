e603c28fee389b0bb3a9320ffc598363590f216d
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-01-17 02:00:02 -0600

    feat(utils): implement file manipulation functions
    I've implemented the following functions:
    
    	* getFileName(url: string): string
    	* getExtension(fileName: string): string
    	* getLangIcon(url: string): string
    
    And the interface IconLookUp:
    
    	[key: string]: {
    	  icon: string
    	}
    
    This functions are used by CodeView component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,45 @@
 export const buildURL = (lang: string, page: string, endpoint: string) => {
   page = page.replace(/\s+/g, '-');
-  
+
   if (lang === "es") {
     return `${endpoint}/${page}`;
   }
 
   return `${lang}/${endpoint}/${page}`;
+}
+
+export const getFileName = (url: string): string => {
+
+  const urlParts = url.split('/');
+
+  return urlParts.pop() || "";
+}
+
+export const getExtension = (fileName: string): string => {
+  const fileNameParts = fileName.split('.');
+
+  return fileNameParts.pop() || "";
+}
+
+interface IconLookUp {
+  [key: string]: {
+    icon: string;
+  };
+}
+
+export const getLangIcon = (url: string): string => {
+  const icons: IconLookUp = {
+    "html": { icon: "fa-html5" },
+    "css": { icon: "fa-css3" },
+    "js": { icon: "fa-js" },
+    "py": { icon: "fa-python" },
+    "java": { icon: "fa-java" },
+    "cpp": { icon: "fa-cpp" },
+    "cs": { icon: "fa-csharp" },
+    "rb": { icon: "fa-ruby" },
+    "php": { icon: "fa-php" },
+    "swift": { icon: "fa-swift" },
+  };
+
+  return icons[getExtension(url)].icon ?? "";
 }
