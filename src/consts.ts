673cd4f17db8174b90dcc9e7e8d7b4ef28daec8d
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-02-13 00:04:17 -0600

    update(consts): update content
    Add FinalProjectImages constant that contains images
    data information for the Carrousel component usage
    into the ProyectoFinal article

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,22 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import { type Image } from '@/types'
+
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const FinalProjectImages: Image[] = [
+  {
+    url: "/images/final-project-main-menu.png",
+    caption: "Captura de pantalla del menú principal del proyecto final."
+  },
+  {
+    url: "/images/final-project-question-page.png",
+    caption: "Captura de pantalla de la página de preguntas del proyecto final."
+  },
+  {
+    url: "/images/final-project-result.png",
+    caption: "Captura de pantalla del resultado del proyecto final."
+  }
+]
