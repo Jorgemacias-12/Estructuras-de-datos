10f1cd3d3546e89febc23f94d8a0d8bd3e4762cd
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-02-13 23:52:55 -0600

    chore(config): update astro config
    Update configuration file to inclue production check to change the
    base url and the site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jorgemacias12.github.io/',
+  base: isProduction ? 'Estructuras-de-datos' : '/',
   integrations: [react(), mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: 'es',
