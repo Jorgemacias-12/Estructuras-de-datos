58ff326375396a1ff4dfca6b6a04789b91ff0576
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-03-01 17:52:22 -0600

    chore(config): update site value
    And also remove base value and see if it works!

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from "@astrojs/react";
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  site: 'https://jorgemacias12.github.io/',
-  base: isProduction ? 'Estructuras-de-datos' : '/',
+  site: 'https://jorgemacias12.github.io/Estructura-de-datos',
+  //base: isProduction ? 'Estructuras-de-datos' : '/',
   integrations: [react(), mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: 'es',
