aaf8b1ce669194287773dce92c8cd537e383efc1
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-06-10 15:43:50 -0600

    update(components): CodeView
    improve monaco references to avoid import errors, also define my own
    theme to fit the background color of the theme light/dark.

--- src/components/CodeView.tsx
+++ src/components/CodeView.tsx
@@ -2,6 +2,9 @@ import React, { useRef } from 'react';
 import { Editor, type Monaco, type OnMount } from '@monaco-editor/react';
 import { getLangIcon } from '../utils';
 import type monaco from 'monaco-editor'
+import { useStore } from '@nanostores/react';
+import { $theme } from '@/stores/theme';
+import { ThemeSwitcher } from './ThemeSwitcher';
 
 type CodeViewProps = {
   fileName?: string;
@@ -13,37 +16,42 @@ type CodeViewProps = {
 };
 
 export const CodeView: React.FC<CodeViewProps> = ({ language, code, readonly, fileName, handleClick, line }: CodeViewProps) => {
-
+  const theme = useStore($theme);
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
-  const handleEditorDidMount = (editor: monaco.editor.IStandaloneCodeEditor , monaco: Monaco) => {
+  const handleEditorDidMount = (editor: monaco.editor.IStandaloneCodeEditor, monaco: Monaco) => {
     editorRef.current = editor;
 
-    if (line && line > 0 ) {
+    if (line && line > 0) {
       editor.revealLineInCenter(line);
     }
   }
 
+  const commonClassNames = "fixed top-0 left-0 flex flex-col justify-center w-full z-10";
+  const containerLight = "bg-white-smoke-700";
+  const containerDark = "bg-raisin-black";
+
   return (
-    <section className="fixed top-0 left-0 flex flex-col justify-center w-full bg-surface-100 z-10">
-      
+    <section className={`${commonClassNames} ${theme === 'light' ? containerLight : containerDark}`}>
+
       <section className="flex justify-between items-center p-4 bg-primary-100">
+        <ThemeSwitcher />
         <h3 className="flex items-center justify-center font-bold text-center flex-grow gap-2">
           {fileName}
           {
             fileName && <span className={`fab ${getLangIcon(fileName)} fa-xl`}></span>
           }
         </h3>
-        <span onClick={handleClick} className="fas fa-times fa-xl hover:text-black transition active:text-red-500"></span>
+        <span onClick={handleClick} className="fas fa-times fa-xl hover:text-red-400 transition active:text-red-500 cursor-pointer"></span>
       </section>
-      
-      <section className="p-4"> 
+
+      <section className="p-4">
         <Editor
           height="100vh"
           width=""
           language={language}
           value={code}
-          theme="vs-dark"
+          theme={theme === 'light' ? 'light' : 'jamz'}
           options={{
             readOnly: readonly,
             minimap: {
