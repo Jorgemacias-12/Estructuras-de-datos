e32224c82e7de813e293624e69397848d734cdd2
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-06-10 03:17:12 -0600

    fix(stores): theme
    Fix window not defined error using undefined check and default
    value

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,7 +1,11 @@
 import type { Theme } from "@/types";
 import { persistentAtom } from "@nanostores/persistent";
 
-const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-const defaultTheme: Theme = userPrefersDark ? 'dark' : 'light';
+export const $theme = persistentAtom<Theme>("theme", "light");
 
-export const $theme = persistentAtom<Theme>("theme", defaultTheme);
+if (typeof window !== 'undefined') {
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const defaultTheme = userPrefersDark ? 'dark' : 'light';
+
+  $theme.set(defaultTheme);
+}
