e9b63191a9c2c966af96479cd6a039fbb629b3e6
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-06-10 20:47:43 -0600

    update(components): Capture
    Include functionality to change backgrounds depending of the current
    Theme.

--- src/components/Capture.tsx
+++ src/components/Capture.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { $theme } from "@/stores/theme";
+import { useStore } from "@nanostores/react";
 
 type CaptureProps = {
   title: string,
@@ -7,14 +8,19 @@ type CaptureProps = {
 }
 
 export const Capture = ({title, content, imageURL}: CaptureProps) => {
-  return (
-    <article className="bg-surface-300 rounded-md">
-      
-      <img src={imageURL} alt="" className="p-4" />
+  const theme = useStore($theme);
+  
+  const light = "bg-white-smoke-500 border";
+  const dark = "bg-raisin-black-600";
 
-      <section className="flex flex-col gap-2 p-4">
+  return (
+    <article className={`flex flex-col gap-4 rounded-md p-2 ${theme === 'light' ? light : dark}`}>
+      
+      <img src={imageURL} alt="" className="" />
+
+      <section className="flex flex-col gap-2">
         <h4 className="font-bold text-xl">{title}</h4>
-        <p>{content}</p>
+        <p className="text-left">{content}</p>
       </section>
     </article>
   )
