ad76d181eb8537975e1aab8744f5795fe50cd9d5
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-06-10 20:57:20 -0600

    update(components): Operation
    Add theme stlying support, and change some text and button styles, and
    also remove old styles.

--- src/components/Operation.tsx
+++ src/components/Operation.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { CodeView } from './CodeView';
 import { getFileName } from '@/utils'
+import { $theme } from '@/stores/theme';
+import { useStore } from '@nanostores/react';
 
 type OperationProps = {
   title: string,
@@ -10,7 +12,7 @@ type OperationProps = {
 }
 
 export const Operation = ({ title, description, codeURL, desiredLine }: OperationProps) => {
-
+  const theme = useStore($theme);
   const [isOpen, setIsOpen] = useState(false);
   const [code, setCode] = useState('');
 
@@ -43,17 +45,21 @@ export const Operation = ({ title, description, codeURL, desiredLine }: Operatio
     document.body.classList.toggle('overflow-hidden')
   }
 
+  const defaultContainerClassNames = "rounded-md border flex flex-col";
+  const containerLight = "bg-white-smoke-500";
+  const containerDark = "bg-raisin-black-600 border-0";
+
   return (
-    <article className="bg-surface-300 border border-surface-500 rounded-md flex flex-col">
+    <article className={`${defaultContainerClassNames} ${theme === 'light' ? containerLight : containerDark}`}>
       <h3 className="text-2xl text-primary-500 p-4 pb-0">{title}</h3>
 
       <section>
         {description.split("#").map((paragraph, index) => {
-          return <p key={index} className="px-4 py-2" dangerouslySetInnerHTML={{ __html: paragraph }}></p>
+          return <p key={index} className="px-4 py-2 text-left" dangerouslySetInnerHTML={{ __html: paragraph }}></p>
         })}
       </section>
 
-      <button type="button" className="bg-primary-100 py-2 rounded-md mx-4 mb-4" onClick={handleClick}>
+      <button type="button" className="bg-indigo-500 border text-white dark:border-raisin-black-600 py-2 rounded-md mx-4 mb-4" onClick={handleClick}>
         {isOpen ? 'Esconder código' : 'Mostrar código'}
       </button>
 
