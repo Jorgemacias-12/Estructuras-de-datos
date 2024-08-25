79904d2c6e9ada52978aa57b18c7a575bb0b26bd
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-08-25 00:43:00 -0600

    update: header remove lang selector component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import { useStore } from "@nanostores/react"
 import { useState } from "react";
 import { getI18N } from "@/i18n";
 import { ThemeSwitcher } from "./ThemeSwitcher";
-import { LangSelector } from "./LangSelector";
 
 const lang = getI18N({ currentLocale: "" })
 const { HEADER_LINKS } = lang
@@ -56,7 +55,6 @@ export const Header = () => {
             }
 
             <ThemeSwitcher />
-            <LangSelector />
           </ul>
         </nav>
       </section>
