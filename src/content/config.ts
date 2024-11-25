b647c37ab7ff9271a30692a9a5bfb85ee79c9740
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-11-25 02:56:53 -0600

    update: add lang prop to schema (activities)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+import { filterByLanguage } from '@/utils';
 import { defineCollection, z } from 'astro:content';
 
 const activities = defineCollection({
@@ -6,11 +7,12 @@ const activities = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		lang: z.string().optional()
 	}),
 });
+
 
 export const collections = { activities };
